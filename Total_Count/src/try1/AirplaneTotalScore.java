b2f3d60d653bd2b2bcad849d6149ba3e64c010d2
hyunjin4270 <hyunn4270@gmail.com>
2024-04-12 20:05:18 +0900

    FIX: Method comparePlaneCount fixed

--- Total_Count/src/try1/AirplaneTotalScore.java
+++ Total_Count/src/try1/AirplaneTotalScore.java
@@ -3,34 +3,62 @@ package try1;
 import java.util.Vector;
 
 public class AirplaneTotalScore {
-    private Vector planeCount = new Vector();
+    private AirplaneTotalScore() {}
+    private static Vector planeCount = new Vector();
 
-    public void comparePlaneCount(String airplane) {
+    
+    public static void comparePlaneCount(String airplane) {
         if (planeCount.size() == 0) {
             planeCount.add(new Airplane(airplane));
             return;
         }
 
-        for (int i = 0; i < planeCount.size(); i++) {
+        for (int i = 0, count = 0; i < planeCount.size(); i++) {
             Airplane compareAirplane = (Airplane) planeCount.get(i);
-            if (compareAirplane.airplaneName.equals(airplane)) {
+
+            if (compareAirplane.AIRPLANE_NAME.equals(airplane)) {
+                Airplane airplane2 = (Airplane) planeCount.get(i);
+                planeCount.remove(i);
+                airplane2.pluskillScore();
+                planeCount.add(airplane2);
+            } else {
+                count++;
+            }
+
+            if (count == planeCount.size()) {
                 planeCount.add(new Airplane(airplane));
             }
         }
     }
 
+    public static void getTotalScore() {
+        for (int i = 0; i < planeCount.size(); i++) {
+            Airplane printAirplane = (Airplane) planeCount.get(i);
+            System.out.println();
+
+        }
+    }
 
 
-    class Airplane extends Object {
-        final String airplaneName;
-        int killScore = 0;
+    private static class Airplane {
+        private final String AIRPLANE_NAME;
+        private int killScore = 0;
 
-        Airplane(String airplaneName) {
-            this.airplaneName = airplaneName;
+        Airplane(String AIRPLANE_NAME) {
+            this.AIRPLANE_NAME = AIRPLANE_NAME;
             pluskillScore();
         }
-        void pluskillScore() {
+        
+        private void pluskillScore() {
             this.killScore++;
         }
+
+        public String getAIRPLANE_NAME() {
+            return AIRPLANE_NAME;
+        }
+        public int getKillScore() {
+            return killScore;
+        }
+
     }
 }
