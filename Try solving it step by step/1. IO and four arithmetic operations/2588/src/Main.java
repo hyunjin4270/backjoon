30e31a420794d067ed0f8373bef5e659182429b7
hyunjin4270 <hyunn4270@gmail.com>
2024-04-27 19:56:11 +0900

    0/27 Beakjoon studied

---
+++ Try solving it step by step/1. IO and four arithmetic operations/2588/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println((B % 10) * A);
+        System.out.println((((B % 100) - (B % 10)) / 10) * A);
+        System.out.println(((B - (B % 100)) / 100) * A);
+        System.out.println(A * B);
+    }
+    
+}
