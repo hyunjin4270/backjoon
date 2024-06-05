4bc24700e81ee7fe733bd1ec33878ab1a536dfbb
hyunjin4270 <hyunn4270@gmail.com>
2024-06-05 16:51:18 +0900

    improve 3004

--- workbook/src/_2966/Main.java
+++ workbook/src/_2966/Main.java
@@ -2,7 +2,6 @@ package _2966;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
 
--- workbook/src/_3004/Main.java
+++ workbook/src/_3004/Main.java
@@ -1,10 +1,16 @@
 package _3004;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(cutWidthAndHeight(scanner.nextInt()));
     }
-    void cutWidthAndHeight(int cu) {
-        //if()
+    static int cutWidthAndHeight(int numberOfCuts) {
+        int Width = (numberOfCuts / 2) + 1;
+        int Height = Width + (numberOfCuts % 2);
+        return Width * Height;
+
     }
 }
